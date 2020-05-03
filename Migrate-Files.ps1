e25b71b02efe5a07e876011ccb9061091d9e2db4
Sergong <serge.meeuwsen@gmail.com>
2020-05-03 19:51:24 +0100

    Fixed path logic for source and destination, logs

--- Migrate-Files.ps1
+++ Migrate-Files.ps1
@@ -1,28 +1,36 @@
-$max_jobs = 10
+<#
+    Example Robocopy Script
+#>
+param(
+    [Parameter(Mandatory=$true)]
+    $src,
+    [Parameter(Mandatory=$true)]
+    $dest
+)
+# Strip trailing \
+if($src.EndsWith("\")){ $src = $src.Substring(0,$src.Length -1) }
+if($dest.EndsWith("\")){ $dest = $dest.Substring(0,$dest.Length -1) }
+
+$log = ".\robologs"
+
+$max_jobs = 2
 $tstart = get-date
-$log = "c:\robo\Logs"
-
-$src = Read-Host -Prompt 'Source path'
-if(! ($src.EndsWith("\") )){$src=$src + "\"}
-$dest = Read-Host -Prompt 'Destination path'
-if(! ($dest.EndsWith("\") )){$dest=$dest + "\"}
-
 
 if((Test-Path -Path $src ))
 {
 	if(!(Test-Path -Path $log )){New-Item -ItemType directory -Path $log}
 	if((Test-Path -Path $dest)){
-		# robocopy $src $dest
+		robocopy "$src" "$dest"
 		$files = Get-ChildItem -Path $src -Directory
 		
 		$files | %{
 			$ScriptBlock = {
                 param($name, $src, $dest, $log)
-                $log += "\$name-$(get-date -f yyyy-MM-dd-mm-ss).log"
-                Write-Host "Starting Copy of $src$name..." -ForegroundColor Yellow
-                robocopy $src$name $dest$name /MIR /COPYALL /NP /MT:8 /LOG+:$log
-                Write-Host "$src$name completed" -ForegroundColor Green
-			 }
+                $log += "\$name-$(get-date -f yyyy-MM-dd-HH-mm-ss).log"
+                Write-Host "Starting Copy of $src\$name..." -ForegroundColor Yellow
+				robocopy "$src\$name" "$dest\$name" /MIR /COPYALL /SECFIX /R:1 /W:5 /ZB /MT:4 /LOG+:"$log"
+                Write-Host "$src\$name completed" -ForegroundColor Green
+			}
 			$j = Get-Job -State "Running"
 			while ($j.count -ge $max_jobs) 
 			{
@@ -31,6 +39,7 @@ if((Test-Path -Path $src ))
 			}
 			Get-job -State "Completed" | Receive-job
 			Remove-job -State "Completed"
+			$log = (get-item -path $log).FullName
 			Start-Job $ScriptBlock -ArgumentList $_,$src,$dest,$log
 		}
 
@@ -39,7 +48,7 @@ if((Test-Path -Path $src ))
 		  Get-Job | Write-host
 
 		$tend = get-date
-		Cls
+		
 		Write-host "Completed copy"
 		Write-host "From: $src"
 		Write-host "To: $Dest"
