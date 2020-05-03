aad292e61e4c8565c3f4c536a80b8c036be12692
Sergong <serge.meeuwsen@gmail.com>
2020-05-03 18:29:04 +0100

    Update error logging and progress indicator

--- Check-Files.ps1
+++ Check-Files.ps1
@@ -26,6 +26,7 @@ Function AddToLog {
 }
 
 $ErrorLog = ".\Check-Files-Error.log"
+
 # Strip trailing '\' if it is passed along
 if($src.Substring($src.Length -1) -eq "\"){
     $src = $src.Substring(0,$src.Length -1)
@@ -36,25 +37,23 @@ if($dst.Substring($dst.Length -1) -eq "\"){
 
 write-host "Gathering Source Paths..." -ForegroundColor Yellow
 $srcPaths = gci $src -Recurse -ErrorVariable +ErrVar -ErrorAction SilentlyContinue
+
+write-host "Gathering Destination Paths..." -ForegroundColor Yellow
+$dstPaths = gci $dst -Recurse -ErrorVariable +ErrVar -ErrorAction SilentlyContinue
+
 if($null -ne $errVar){
     $ErrorMsg = "One or more errors occurred, these could be access privilege related, please check $ErrorLog"
     write-host $ErrorMsg -fore Red
     write-output $errVar | out-file $ErrorLog
 }
 
-write-host "Gathering Destination Paths..." -ForegroundColor Yellow
-$dstPaths = gci $dst -Recurse -ErrorVariable +ErrVar -ErrorAction SilentlyContinue
-if($null -ne $errVar){
-    $ErrorMsg = "One or more errors occurred, these could be access privilege related, please check $ErrorLog"
-    write-host $ErrorMsg -fore Red
-    write-output $errVar | out-file $ErrorLog -Append
-}
-
 # Check file counts
 if($srcPaths.Count -ne $dstPaths.Count){
     write-host "Number of Files in Source and Destination does not match!" -ForegroundColor Red
-    write-host "Source      : $($srcPaths.Count)" -ForegroundColor Red
-    write-host "Destination : $($dstPaths.Count)" -ForegroundColor Red
+    write-host "Source        : $($srcPaths.Count)" -ForegroundColor Red
+    write-host "Destination   : $($dstPaths.Count)" -ForegroundColor Red
+    $Diff = $srcPaths.Count - $dstPaths.Count
+    write-host "Missing files : $Diff" -ForegroundColor Red
 } else {
     write-host "File counts of Source and Destination match!" -ForegroundColor Green
 }
@@ -63,22 +62,21 @@ if($srcPaths.Count -ne $dstPaths.Count){
 # Check file Hashes 
 $LogObj = @()
 $SampleSet = $srcPaths | Get-Random -Count $SampleSize
+$c = 1
 foreach($file in $SampleSet){
+    Write-Progress  -Activity "Processing $($file.FullName)" -PercentComplete (100 * ($c / $SampleSet.Count)) -Status "Comparing file hashes of $($SampleSet.Count) paths..."
     $srcFile = $file.FullName
     $srcHash = (Get-FileHash $srcFile).hash
-    #write-host "Source Path: $srcFile" -fore Cyan
 
     $dstFile = $file.FullName.Replace($src,$dst)
-    #write-host "Dest Path:   $dstFile" -fore Cyan
-
     $dstHash = (Get-FileHash $dstFile).hash
+
     if ($srcHash -ne $dstHash){
-        #write-host "The hash of the source file $($file.FullName) did not match the destination file hash!" -fore Red
         $LogObj += AddToLog -Source $file.FullName -Status "NOK"
     } else {
-        #write-host "The hash of the source file $($file.FullName) matched the destination file hash." -fore Green
         $LogObj += AddToLog -Source $file.FullName -Status "OK"
     }
+    $c++
 }
 
 # Output Exceptions if there are any
@@ -87,6 +85,6 @@ if($Null -ne $OutCsv){
     write-host "Some exceptions found, please check $OutFile" -ForegroundColor Red
     $OutCsv| Export-Csv -notypeinformation -path $OutFile
 } else {
-    write-host "No exception found, All SHA256 hashed matched." -ForegroundColor Green
+    write-host "No exception found, the SHA256 checksum of all $sampleSize sampled files matched." -ForegroundColor Green
 }
 
