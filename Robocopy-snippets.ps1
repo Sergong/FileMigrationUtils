dc8b3d9e618fdf0b88d71afb3148bd7e6a5868bf
Serge Meeuwsen <smeeuwsen@sme-fc30.local>
2020-05-12 20:16:30 +0100

    Turned into Function

--- Robocopy-snippets.ps1
+++ Robocopy-snippets.ps1
@@ -4,12 +4,24 @@
 
 #>
 
-# this should give you total foldersize in bytes
-$folder = "c:\program files"
-$totbytes = (robocopy $folder "c:\temp" /zb /e /l /r:1 /w:1 /nfl /ndl /nc /fp /bytes /np /njh | ? { $_ -match "Bytes :"}).trim().split(" ")[2]
+function Calculate-FolderSize() {
+    param(
+        # Parameter help description
+        [Parameter(Mandatory=$true)]
+        [string]
+        $folder
+    )
 
-write-host "$folder is $([math]::Round(($totbytes / 1GB ),2)) GB in size"
+    # this should give you total foldersize in bytes
+    $totbytes = (robocopy $folder $env:temp /zb /e /l /r:1 /w:1 /nfl /ndl /nc /fp /bytes /np /njh |  Where-Object{ $_ -match "Bytes :"}).trim().split(" ")[2]
+    Return [math]::Round(($totbytes / 1GB ),2)
+}
 
-# send the bytes to log file
+
+# write-host "$folder is $() GB in size"
+
+<# send the bytes to log file
+
 robocopy "large path name" "c:\temp" /zb /e /l /r:1 /w:1 /ndl /nfl /bytes /np /njh /log:size.log
 
+#>
