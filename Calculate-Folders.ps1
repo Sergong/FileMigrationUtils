8197d65b092f31cdda9a1df9658f569796fa7b7b
Serge Meeuwsen <serge.meeuwsen@gmail.com>
2020-05-17 03:43:18 +0100

    Refactored to oupot PSObject

--- Calculate-Folders.ps1
+++ Calculate-Folders.ps1
@@ -1,36 +1,45 @@
+#Requires -Version 2
 <#
 
-    This contains handy robocopy tricks
+    Robocopy wrapper for File Migration Planning
+    Calculates the total number of files/folder and size in GB
 
 #>
 param(
-    [Parameter(Mandatory=$true)]
-    [string]
-    $folder,
-    [Parameter(Mandatory=$false)]
-    [string]
-    $Log
+	# Parameter help description
+	[Parameter(Mandatory=$true)]
+	[string]
+	$folder,
+	[Parameter(Mandatory=$false)]
+	[string]
+	$Log = $null
 )
-function Get-FolderSize() {
-    param(
-        # Parameter help description
-        [Parameter(Mandatory=$true)]
-        [string]
-        $folder
-    )
-
-    # this should give you total foldersize in bytes
-    $totbytes = (robocopy $folder $env:temp /zb /e /l /r:1 /w:1 /nfl /ndl /nc /fp /bytes /np /njh |  Where-Object{ $_ -match "Bytes :"}).trim().split(" ")[2]
-    Return [math]::Round(($totbytes / 1GB ),2)
+if($log -eq ""){
+	$Drive = $folder.substring(0,1)
+	$t = $folder.split("\")
+	$Log = ".\$Drive-$($t[$t.count-1]).log"
 }
 
-robocopy $folder $env:temp /zb /e /l /r:1 /w:1 /ndl /nfl /bytes /np /njh /log:$Log
+write-host "Running Robocopy in Virtual Mode on $folder..." -fore Yellow
+$null = robocopy $folder "c:\temp" /zb /e /l /r:1 /w:1 /ndl /nfl /bytes /np /njh /log:$Log
+$Content = cat $log
+$Dirs = ($Content | Where-Object{ $_ -match "Dirs :"}).trim()
+$tmp = $Dirs -match 'Dirs :\s+([0-9]+)'
+$Dirs = $Matches[1]
+$Files = ($Content | Where-Object{ $_ -match "Files :"}).trim()
+$tmp = $Files -match 'Files :\s+([0-9]+)'
+$Files = $Matches[1]
+$Bytes = ($Content | Where-Object{ $_ -match "Bytes :"}).trim().split(" ")[2]
 
-# write-host "$folder is $() GB in size"
+$record = "" | select Folders, Files, SizeGB
+$record.Folders = "{0:N}" -f $Dirs
+$record.Files = "{0:N}" -f $Files
+$record.SizeGB = "{0:N2}" -f [math]::round($Bytes/1GB,2)
 
-<# send the bytes to log file
 
+$record
 
+<#
 
 Switches Used:
 /zb     Use restartable mode, if access denied use Backup mode
